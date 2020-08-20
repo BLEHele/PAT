466a0719e177446b720fce54363b707e97831a46
BLEHele <54165962+BLEHele@users.noreply.github.com>
2020-08-20 20:56:37 +0800

    Add files via upload

---
+++ 1019 General Palindromic Number.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	int n, c, a[27], b[27], count = 0, flag = 1, i;
+	cin >> n >> c;
+	while (n > 0) {
+		a[count] = n % c;
+		n /= c;
+		count++;
+	}
+	for (i = 0; i < count; i++) {
+		b[i] = a[count - i - 1];
+	}
+	for (i = 0; i < count; i++) {
+		if (a[i] != b[i]) {
+			flag = 0;
+			break;
+		}
+	}
+	if (flag == 1)	cout << "Yes" << endl;
+	else
+	{
+		cout << "No" << endl;
+	}
+	cout << b[0];
+	for (i = 1; i < count; i++) {
+		cout << " " << b[i];
+	}
+	return 0;
+}
