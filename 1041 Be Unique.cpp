582ca1d12157d3d97df5b269ff703bb85f53e2ad
BLEHele <54165962+BLEHele@users.noreply.github.com>
2020-08-21 17:48:48 +0800

    Add files via upload

---
+++ 1041 Be Unique.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	int n, i, num[10000] = { 0 }, a[100001], t;
+	cin >> n;
+	for (i = 0; i < n; i++) {
+		cin >> a[i];
+		num[a[i]]++;
+	}
+	for (i = 0; i < n; i++) {
+		if (num[a[i]] == 1) {
+			t = a[i];
+			break;
+		}
+	}
+	if (i == n)	cout << "None";
+	else {
+		cout << t;
+	}
+	return 0;
+}
