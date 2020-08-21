0c67315ec278853926ad5bb0d8a12f7d255fa535
BLEHele <54165962+BLEHele@users.noreply.github.com>
2020-08-21 18:43:13 +0800

    Add files via upload

---
+++ 1050 String Subtraction.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+	string s1, s2;
+	int i, num[128] = { 0 };
+	getline(cin, s1);
+	getline(cin, s2);
+	for (i = 0; i < s2.size(); i++) {
+		num[(int)s2[i]] = 1;
+	}
+	for (i = 0; i < s1.size(); i++) {
+		if (num[(int)s1[i]] == 0)
+			cout << s1[i];
+	}
+	return 0;
+}
