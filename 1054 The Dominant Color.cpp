fd78d041b372a1ab58c670073fdde9a5e32d203b
BLEHele <54165962+BLEHele@users.noreply.github.com>
2020-08-21 20:47:27 +0800

    Add files via upload

---
+++ 1054 The Dominant Color.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include<map>
+using namespace std;
+int main() {
+	int i, m, n, t;
+	cin >> m >> n;
+	map<int, int>CountColor;
+	for (i = 0; i < m * n; i++) {
+		scanf_s("%d", &t);
+		CountColor[t]++;
+		if (CountColor[t] > m * n / 2)	break;
+	}
+	cout << t;
+	return 0;
+}
